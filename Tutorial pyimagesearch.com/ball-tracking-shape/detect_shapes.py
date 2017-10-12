ed4b163e33e2e07ba423efbf4e6b3c6fa4ce5573
Antonio Garcia <agarciaf7@gmail.com>
2017-10-12 12:27:35 +0200

    Carga inicial

---
+++ Tutorial pyimagesearch.com/ball-tracking-shape/detect_shapes.py
@@ -0,0 +1,65 @@
+# USAGE
+# python detect_shapes.py --picamera 1 si es con picamera
+# python detect_shapes.py si es con una webcam normal
+
+# import the necessary packages
+from pyimagesearch.shapedetector import ShapeDetector
+import argparse
+import imutils
+import cv2
+from imutils.video import VideoStream
+import time
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--picamera", type=int, default=-1,
+	help="whether or not the Raspberry Pi camera should be used")
+args = vars(ap.parse_args())
+
+camera = VideoStream(usePiCamera=args["picamera"] > 0).start()
+time.sleep(2.0)
+
+# keep looping
+while True:
+	# grab the current frame
+	image = camera.read()
+
+	# resize the image to a smaller factor so that
+	# the shapes can be approximated better
+	resized = imutils.resize(image, width=300)
+	ratio = image.shape[0] / float(resized.shape[0])
+
+	# convert the resized image to grayscale, blur it slightly,
+	# and threshold it
+	gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+	blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+	thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
+	# find contours in the thresholded image and initialize the
+	# shape detector
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+	sd = ShapeDetector()
+
+	# loop over the contours
+	for c in cnts:
+		# compute the center of the contour, then detect the name of the
+		# shape using only the contour
+		M = cv2.moments(c)
+		cX = int((M["m10"] / M["m00"]) * ratio)
+		cY = int((M["m01"] / M["m00"]) * ratio)
+		shape = sd.detect(c)
+
+		# multiply the contour (x, y)-coordinates by the resize ratio,
+		# then draw the contours and the name of the shape on the image
+		c = c.astype("float")
+		c *= ratio
+		c = c.astype("int")
+		cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+		cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+			0.5, (255, 255, 255), 2)
+
+		# show the output image
+		cv2.imshow("Image", image)
+		cv2.waitKey(0)
